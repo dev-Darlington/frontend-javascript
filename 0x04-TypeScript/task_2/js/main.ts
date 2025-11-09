c596f5a9aaa408bf1694b9061612ad7510203374
Darlington <darlingtonuchenna53@gmail.com>
2025-11-09 22:58:35 +0100

    updated main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,10 +1,18 @@
+function workDirectorTasks () {
+    return "Getting to director tasks";
+}
+function workTeacherTasks () {
+    return "Getting to work";
+}
 export function isDirector(employee: { role: string }): boolean {
     return employee.role === 'Director';
 }
 function executeWork(employee: { role: string }): void {
+    isDirector(employee);
     if(employee.role === 'Director') {
-        workDirectorTasks();
+        return employee.workDirectorTasks();
     } else {
-        workTeacherTasks();
+        return employee.workTeacherTasks();
     }
+
 }
