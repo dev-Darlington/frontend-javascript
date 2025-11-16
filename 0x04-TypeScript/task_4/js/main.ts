4d7c2fbe0e550912688376fd55cde6001a40468c
Darlington <darlingtonuchenna53@gmail.com>
2025-11-16 13:19:49 +0100

    updated constants in main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -2,5 +2,7 @@ export const react = new React();
 export const java = new Java();
  export const cpp = new Cpp();
  export const cTeacher: Teacher = {
+    firstName: "John",
+    lastName: "Doe",
     experienceTeachingC: 10,
  }
